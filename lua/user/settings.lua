fa16fe0c938e3cc44db67932a83a4719f3318fdd
smoralesd <smoralesd@users.noreply.github.com>
2022-10-08 14:23:57 -0700

    Update settings

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,16 +1,17 @@
-vim.opt.number = true			-- show numbers on each line
+vim.opt.number = true			      -- show numbers on each line
 vim.opt.relativenumber = true		-- use relative numbers by default
 
 vim.opt.termguicolors = true 		-- enables 24-bit colors
 
-vim.opt.cursorline = true		-- highlight the line the cursor is in
+vim.opt.cursorline = true		    -- highlight the line the cursor is in
 
-vim.opt.wrap = false		  	-- disable line wrapping
+vim.opt.wrap = false		  	    -- disable line wrapping
 
 vim.opt.backup = false
 vim.opt.swapfile = false
 vim.opt.hidden = true
 
-vim.opt.splitbelow = true	 	-- split a new window below/to the right by default
+vim.opt.splitbelow = true	 	    -- split a new window below/to the right by default
+vim.opt.signcolumn = 'yes'      -- always show the sign/gutter column so the content does not shift
 
 vim.cmd[[colorscheme tokyonight-storm]]
