11c995532880f560fce261b2fecaba680a038ea8
smoralesd <smoralesd@users.noreply.github.com>
2022-10-08 09:08:44 -0700

    Add common settings

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,1 +1,16 @@
+vim.opt.number = true			-- show numbers on each line
+vim.opt.relativenumber = true		-- use relative numbers by default
+
+vim.opt.termguicolors = true 		-- enables 24-bit colors
+
+vim.opt.cursorline = true		-- highlight the line the cursor is in
+
+vim.opt.wrap = false		  	-- disable line wrapping
+
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.hidden = true
+
+vim.opt.splitbelow = true	 	-- split a new window below/to the right by default
+
 vim.cmd[[colorscheme tokyonight-storm]]
