1800721eaaf7a4691c54ad1f2bd6ab3e87b05e49
smoralesd <smoralesd@users.noreply.github.com>
2022-10-09 10:44:17 -0700

    Setting: showmode = false, since lualine shows the mode already

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,17 +1,19 @@
-vim.opt.number = true			      -- show numbers on each line
-vim.opt.relativenumber = true		-- use relative numbers by default
+vim.opt.number = true -- show numbers on each line
+vim.opt.relativenumber = true -- use relative numbers by default
 
-vim.opt.termguicolors = true 		-- enables 24-bit colors
+vim.opt.termguicolors = true -- enables 24-bit colors
 
-vim.opt.cursorline = true		    -- highlight the line the cursor is in
+vim.opt.cursorline = true -- highlight the line the cursor is in
 
-vim.opt.wrap = false		  	    -- disable line wrapping
+vim.opt.wrap = false -- disable line wrapping
 
 vim.opt.backup = false
 vim.opt.swapfile = false
 vim.opt.hidden = true
 
-vim.opt.splitbelow = true	 	    -- split a new window below/to the right by default
-vim.opt.signcolumn = 'yes'      -- always show the sign/gutter column so the content does not shift
+vim.opt.splitbelow = true -- split a new window below/to the right by default
+vim.opt.signcolumn = 'yes' -- always show the sign/gutter column so the content does not shift
 
-vim.cmd[[colorscheme tokyonight-storm]]
+vim.opt.showmode = false -- mode is shown via lualine
+
+vim.cmd [[colorscheme tokyonight-storm]]
