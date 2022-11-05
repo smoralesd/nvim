6caf19e65b62f4478642259de53528e6912392db
smoralesd <smoralesd@users.noreply.github.com>
2022-11-04 19:36:43 -0700

    Remove extra space

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -109,7 +109,7 @@ return packer.startup(function(use)
   use { "akinsho/toggleterm.nvim", tag = '*' }
 
   -- keybindings helpers
-  use  "folke/which-key.nvim";
+  use "folke/which-key.nvim";
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
