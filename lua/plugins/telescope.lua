8a2ce95fd67c4bc82abeb42aaa63cf52e2182ca3
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-01-11 14:13:44 -0800

    Add/update telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,8 +7,18 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
 
-      vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-      vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = 'Find files' })
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[s]earch via [g]rep" })
+      vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[s]earch [b]uffer" })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[s]earch [d]iagnostics' })
+      vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] find recently opened files' })
+      vim.keymap.set('n', '<leader>/', function()
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] fuzzily search in current buffer' })
     end,
   },
   {
