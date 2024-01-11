b75910999a924ff924a71d38194a832503fc52ba
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-01-11 14:42:53 -0800

    Add/update lsp keymaps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,35 +1,43 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
-			lspconfig.tsserver.setup({
+      lspconfig.tsserver.setup({
         capabilities = capabilities
       })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "hover documentation" })
+      vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, { desc = 'signature documentation' })
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = '[r]e[n]ame' })
+      vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = '[r]e[n]ame' })
+
+      local builtin = require('telescope.builtin')
+
+      vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = '[g]oto [d]efinition' })
+      vim.keymap.set("n", "<F12>", builtin.lsp_definitions, { desc = 'goto definition' })
+      vim.keymap.set("n", "gr", builtin.lsp_references, { desc = '[g]oto [r]eferences' })
+      vim.keymap.set("n", "<s-F12>", builtin.lsp_references, { desc = 'goto references' })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = '[c]ode [a]ction' })
+    end,
+  },
 }
