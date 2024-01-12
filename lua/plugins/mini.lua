a8d6c062f1ed647e4225c73a08842ed93509db16
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-01-12 11:41:43 -0800

    Add some mini.x plugins

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'echasnovski/mini.cursorword',
+    version = '0.11.0',
+    opts = {}
+  },
+  {
+    'echasnovski/mini.surround',
+    version = '0.11.0',
+    opts = {}
+  },
+  {
+    'echasnovski/mini.trailspace',
+    version = '0.11.0',
+    opts = {}
+  },
+}
