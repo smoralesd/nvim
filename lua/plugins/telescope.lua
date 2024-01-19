0b5e3d036a316c475a9dee55a04d7f2868784ead
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-01-18 17:26:24 -0800

    Improve filenames on telescope results

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,38 +1,69 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		-- or                              , branch = '0.1.x',
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    -- or                              , branch = '0.1.x',
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[s]earch via [g]rep" })
-			vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[s]earch [b]uffer" })
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[s]earch [d]iagnostics" })
-			vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] find recently opened files" })
-			vim.keymap.set("n", "<leader>/", function()
-				-- You can pass additional configuration to telescope to change theme, layout, etc.
-				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-					winblend = 10,
-					previewer = false,
-				}))
-			end, { desc = "[/] fuzzily search in current buffer" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
+      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[s]earch via [g]rep" })
+      vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[s]earch [b]uffer" })
+      vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[s]earch [d]iagnostics" })
+      vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] find recently opened files" })
+      vim.keymap.set("n", "<leader>/", function()
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+          winblend = 10,
+          previewer = false,
+        }))
+      end, { desc = "[/] fuzzily search in current buffer" })
 
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      local function filenameFirst(_, path)
+        local tail = vim.fs.basename(path)
+        local parent = vim.fs.dirname(path)
+        if parent == "." then return tail end
+        return string.format("%s\t\t%s", tail, parent)
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "TelescopeResults",
+        callback = function(ctx)
+          vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+            vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+          end)
+        end,
+      })
+
+      require("telescope").setup({
+        defaults = {
+          layout_strategy = "horizontal",
+          layout_config = { width = 0.75 }
+        },
+        pickers = {
+          git_status = { path_display = filenameFirst, },
+          find_files = { path_display = filenameFirst, },
+          buffers = { path_display = filenameFirst, },
+          live_grep = { path_display = filenameFirst, },
+          oldfiles = { path_display = filenameFirst, },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
