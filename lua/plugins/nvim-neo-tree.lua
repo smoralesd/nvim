0b4861f783fecd7b378d6ecbb842f742be4e61a2
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-01-18 17:43:41 -0800

    neotree: set up follow current file

--- lua/plugins/nvim-neo-tree.lua
+++ lua/plugins/nvim-neo-tree.lua
@@ -1,17 +1,46 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-		vim.keymap.set("n", "<C-n>", ":Neotree toggle right<CR>", { silent = true })
-	end,
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle right<CR>", { silent = true })
+
+
+    require("neo-tree").setup({
+      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      sort_case_insensitive = true, -- used when sorting files and directories in the tree
+      -- A list of functions, each representing a global custom command
+      -- that will be available in all sources (if not overridden in `opts[source_name].commands`)
+      -- see `:h neo-tree-custom-commands-global`
+      window = {
+        position = "right",
+        width = 50,
+      },
+      filesystem = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --              -- the current file is changed while the tree is open.
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+        group_empty_dirs = true, -- when true, empty folders will be grouped together
+        show_unloaded = true,
+      },
+    })
+  end,
 }
