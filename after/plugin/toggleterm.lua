dbd09db92a089b38e357ba6fd71591c295ac86a5
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-01-19 16:03:32 -0800

    Add toggleterm plugin

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,13 @@
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { buffer = 0, desc = 'Terminal: switch to normal mode' })
+
+  vim.keymap.set('t', '<C-Left>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-Down>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-Up>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-Right>', [[<Cmd>wincmd l<CR>]], opts)
+
+  vim.keymap.set('t', '<F6>', [[<Cmd>ToggleTerm<CR>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://*toggleterm* lua set_terminal_keymaps()')
--- lua/settings.lua
+++ lua/settings.lua
@@ -3,18 +3,29 @@ vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 
-vim.opt.number = true -- show numbers on each line
+vim.opt.number = true         -- show numbers on each line
 vim.opt.relativenumber = true -- use relative numbers by default
 
-vim.opt.cursorline = true -- highlight the line the cursor is in
+vim.opt.cursorline = true     -- highlight the line the cursor is in
 
-vim.opt.wrap = false -- disable line wrapping
+vim.opt.wrap = false          -- disable line wrapping
 
-vim.opt.showmode = false -- mode is shown via lualine
+vim.opt.showmode = false      -- mode is shown via lualine
 
-vim.opt.splitbelow = true -- split a new window below/to the right by default
+vim.opt.splitbelow = true     -- split a new window below/to the right by default
 
-vim.opt.signcolumn = "yes" -- always show the signcolumn
+vim.opt.signcolumn = "yes"    -- always show the signcolumn
 
-vim.opt.ignorecase = true -- ignore case while searching
-vim.opt.smartcase = true -- allows to override ignorecase by using upper case characters while searching
+vim.opt.ignorecase = true     -- ignore case while searching
+vim.opt.smartcase = true      -- allows to override ignorecase by using upper case characters while searching
+
+vim.opt.hidden = true
+
+vim.opt.shell = "pwsh" -- set shell to Powershell Core
+-- see :help shell-powershell
+vim.opt.shellcmdflag =
+"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
