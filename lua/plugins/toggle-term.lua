fc67eff7b038b4f42073c9f56bdae67546e1e346
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-01-19 16:26:45 -0800

    Add custom lazygit toggleterm Terminal

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -2,5 +2,25 @@ return {
   'akinsho/toggleterm.nvim',
 	event = "VeryLazy",
   version = "*",
-  opts = {},
+  config = function ()
+    require("toggleterm").setup({})
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "curved",
+      },
+      hidden = true
+    })
+
+    ---@diagnostic disable-next-line: lowercase-global
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true, desc = "Terminal: open lazygit" })
+  end
 }
