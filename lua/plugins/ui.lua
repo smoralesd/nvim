7729e7db9c2532a19c0ce04a7b7c5aea4679497b
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-02-05 21:51:39 -0800

    Update which-key config
    Add name for groups

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,6 +3,22 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    opts = {
+      plugins = { spelling = true },
+      defaults = {
+        mode = { "n", "v" },
+        ["]"] = { name = "+next" },
+        ["["] = { name = "+prev" },
+        ["<leader>b"] = { name = "+buffer" },
+        ["<leader>f"] = { name = "+file/find" },
+        ["<leader>g"] = { name = "+git" },
+      },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.register(opts.defaults)
+    end,
   },
 
   -- icons
