58510b386e3fb23c5048e26dca0d25b5c2064821
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-02-05 22:27:04 -0800

    Use orange for the line number

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,12 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require("tokyonight").setup({
+      on_highlights = function(hl, c)
+        hl.CursorLineNr = { fg = c.orange }
+      end
+    })
+
     vim.cmd.colorscheme("tokyonight-moon")
   end,
 }
