1f0354c9d760b66c1b064ac1c6ad509f6a39fe0d
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-02-24 15:41:29 -0800

    Add catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,54 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      on_highlights = function(hl, c)
-        hl.CursorLineNr = { fg = c.orange }
-      end,
-    })
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        on_highlights = function(hl, c)
+          hl.CursorLineNr = { fg = c.orange }
+        end,
+      })
+    end,
+  },
 
-    vim.cmd.colorscheme("tokyonight-moon")
-  end,
+  -- catppucin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        neotree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
+    config = function()
+      require("catppuccin").setup({
+        custom_highlights = function(colors)
+          return {
+            CursorLineNr = { fg = colors.peach },
+          }
+        end,
+      })
+
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
 }
