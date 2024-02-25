8f31c0d5d3fbe0a7f811af8a8d51f1112c4f5b75
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-02-25 11:36:14 -0800

    Add terminal utility
    Using the terminal for lazygit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,10 @@
+local terminal = require("util.terminal")
+
 local map = vim.keymap.set
 
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+
+-- lazygit
+map("n", "<leader>gg", function()
+  terminal({ "lazygit" }, { esc_esc = false, ctrl_hjkl = false })
+end, { desc = "Lazygit" })
