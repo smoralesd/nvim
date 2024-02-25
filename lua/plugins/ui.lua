07afceb11509b6e300fda14ee548dc414a65dff9
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-02-24 16:06:20 -0800

    Add mini.indentscope

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -63,6 +63,36 @@ return {
     end,
   },
 
+  -- Active indent guide and indent text objects. When you're browsing
+  -- code, this highlights the current level of indentation, and animates
+  -- the highlighting.
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+
   -- Displays a popup with possible key bindings of the command you started typing
   {
     "folke/which-key.nvim",
