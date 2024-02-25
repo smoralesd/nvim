ff15d0709d504e15f881f45a660a930543009991
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-02-24 21:08:20 -0800

    Add comment-releated plugins
    - nvim-ts-context-commentstring
    - mini.comment

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -138,4 +138,24 @@ return {
     "echasnovski/mini.surround",
     opts = {},
   },
+
+  -- comments
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
+    },
+  },
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        custom_commentstring = function()
+          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    },
+  },
 }
