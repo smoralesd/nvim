6c506d8a2025aa2eca31a4b448b4f0084fc64b97
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-04-10 11:59:31 -0700

    Improve support for json files; Add SchemaStore

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -30,6 +30,7 @@ return {
         "javascript",
         "jsdoc",
         "json",
+        "json5",
         "jsonc",
         "lua",
         "luadoc",
--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,4 +1,11 @@
 return {
   -- library used by other plugins
   { "nvim-lua/plenary.nvim", lazy = true },
+
+  -- json & yaml schema support
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false, -- last release is way too old
+  },
 }
