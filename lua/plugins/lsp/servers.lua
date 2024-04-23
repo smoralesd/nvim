363272f2f238ee7f47357d3e6eaf3b80ad18f453
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-04-23 15:38:41 -0700

    LSP: add powershell support

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -111,4 +111,13 @@ return {
       },
     },
   },
+
+  powershell_es = {
+    bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services/",
+    settings = {
+      init_options = {
+        enableProfileLoading = false,
+      },
+    },
+  },
 }
