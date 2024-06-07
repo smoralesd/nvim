c4358ea4aa6e8b7f9ed1fd7e3caf893cb702ac9f
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-06-07 16:23:18 -0700

    Remove mason build command
    Cross-repo compatibility with dev-box-setup scripts

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -95,7 +95,6 @@ return {
     "williamboman/mason.nvim",
     cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-    build = ":MasonUpdate",
     opts = {
       ensure_installed = {
         "stylua",
