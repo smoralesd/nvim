51288f469014f87b2d6ac76707ce896cc8e32b20
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-08-09 16:55:31 -0700

    Add iamcco/markdown-preview.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,14 +1,35 @@
 return {
-  "MeanderingProgrammer/markdown.nvim",
-  opts = {
-    file_types = { "markdown", "norg", "rmd", "org" },
+  -- Markdown browser preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    ft = { "markdown" },
   },
-  ft = { "markdown", "norg", "rmd", "org" },
-  keys = {
-    {
-      "<leader>um",
-      "<cmd>RenderMarkdown toggle<cr>",
-      desc = "Render Markdown",
+  -- Markdown in-nvim rendering
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    opts = {
+      file_types = { "markdown", "norg", "rmd", "org" },
+    },
+    ft = { "markdown", "norg", "rmd", "org" },
+    keys = {
+      {
+        "<leader>um",
+        "<cmd>RenderMarkdown toggle<cr>",
+        desc = "Render Markdown",
+      },
     },
   },
 }
