c438002a07261871e366bb4ca3e867c5b47618a7
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2024-08-09 16:19:55 -0700

    Remove ellisonleao/glow.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -107,9 +107,6 @@ return {
     end,
   },
 
-  -- Preview markdown code directly in your neovim terminal
-  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
-
   -- indent guides for Neovim
   {
     "lukas-reineke/indent-blankline.nvim",
