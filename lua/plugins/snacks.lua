c07136c1318e141b84834474a047d15720ef2096
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2025-01-31 10:06:34 -0800

    Add a handful of Snack toggles and options

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -16,5 +16,18 @@ return {
         desc = "Git Blame Line",
       },
     },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+          Snacks.toggle.diagnostics():map("<leader>ud")
+          Snacks.toggle.line_number():map("<leader>ul")
+          Snacks.toggle.treesitter():map("<leader>uT")
+        end,
+      })
+    end,
   },
 }
