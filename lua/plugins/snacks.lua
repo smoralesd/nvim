911682d279b92a420740222dc016c92e9a4c6b5f
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2025-02-05 15:33:11 -0800

    Minor update on dashboard sections
    Show recent files considers cwd (only show recent file for the current
    directory)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -46,8 +46,15 @@ return {
             padding = 1,
           },
           { section = "keys", gap = 1, padding = 1 },
-          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Recent Files",
+            section = "recent_files",
+            cwd = true,
+            indent = 2,
+            padding = 1,
+          },
           {
             pane = 2,
             icon = " ",
