73c72b1368cceb558a9d9f6a9d9ca1ffae138ab0
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2025-02-25 13:17:26 -0800

    Clean up commented code; move a couple of git related keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -57,14 +57,6 @@ map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
--- lazygit
--- stylua: ignore
-map("n", "<leader>gg", function() Snacks.lazygit() end, { desc = "Lazygit" })
--- stylua: ignore
-map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Current File History" })
--- stylua: ignore
-map("n", "<leader>gL", function() Snacks.picker.git_log() end, { desc = "Git Log" })
-
 -- quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
