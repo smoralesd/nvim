24dfc3a93b6a29d3d78b85f02b7f3c4c78870eb7
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2025-03-18 14:01:11 -0700

    Add keymaps for rest.nvim

--- lua/plugins/rest_client.lua
+++ lua/plugins/rest_client.lua
@@ -1,5 +1,15 @@
 return {
   {
     "rest-nvim/rest.nvim",
+    init = function()
+      -- keys (from lazy.nvim) does not seem to work with rest.nvim
+      -- using an auto command to create keymaps for the plugin
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "http",
+        callback = function()
+          vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>", { buffer = true, desc = "Run Rest Request" })
+        end,
+      })
+    end,
   },
 }
