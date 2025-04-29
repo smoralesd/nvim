187ee729eb22b28a7961985952d0b9b5e464a83c
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2025-04-29 15:31:51 -0700

    Update lsp config to use blink

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -53,12 +53,12 @@ return {
         require("plugins.lsp.keymaps").on_attach(client, buffer)
       end)
 
-      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local has_blink, blink = pcall(require, "blink.cmp")
       local capabilities = vim.tbl_deep_extend(
         "force",
         {},
         vim.lsp.protocol.make_client_capabilities(),
-        has_cmp and cmp_nvim_lsp.default_capabilities() or {}
+        has_blink and blink.get_lsp_capabilities() or {}
       )
 
       local function setup(server)
