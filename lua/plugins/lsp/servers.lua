6cee3b1eb8c46d0e7ea1a81960a48f404415d097
Sebastian Morales <2837582+smoralesd@users.noreply.github.com>
2025-11-12 17:00:02 -0800

    Remove keys from ts_ls
    It was breaking the configuration

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -13,34 +13,6 @@ return {
   },
 
   ts_ls = {
-    keys = {
-      {
-        "<leader>co",
-        function()
-          vim.lsp.buf.code_action({
-            apply = true,
-            context = {
-              only = { "source.organizeImports.ts" },
-              diagnostics = {},
-            },
-          })
-        end,
-        desc = "Organize Imports",
-      },
-      {
-        "<leader>cR",
-        function()
-          vim.lsp.buf.code_action({
-            apply = true,
-            context = {
-              only = { "source.removeUnused.ts" },
-              diagnostics = {},
-            },
-          })
-        end,
-        desc = "Remove Unused Imports",
-      },
-    },
     settings = {
       completions = {
         completeFunctionCalls = true,
